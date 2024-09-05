e7c2261996c0e8080197840d48189304756eaf4e
Matías Baxman <matiasbaxman@gmail.com>
2024-09-05 14:38:02 -0400

    Agregando app "Login" y "Register" + logica

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
-  constructor() {}
+export class HomePage implements OnInit{
 
   /* Logica de la aplicacion */
+
+  constructor() { }
+
+  ngOnInit() {
+  }
 
 }
