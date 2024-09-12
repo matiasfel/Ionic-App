95ee66ba5bd428af278d82ed583047772fd10142
Matias <matiasbaxman@gmail.com>
2024-09-12 02:58:12 -0300

    add: logic for register page

--- src/app/pages/auth/register/register.page.ts
+++ src/app/pages/auth/register/register.page.ts
@@ -9,6 +9,10 @@ import { AlertController, ToastController } from '@ionic/angular';
 })
 export class RegisterPage {
 
+  user:string=""
+  password: string=""
+  rePassword: string=""
+
   constructor(
     private toastController: ToastController,
     private route: Router,
@@ -16,5 +20,40 @@ export class RegisterPage {
   ) { }
 
   //ToastController y AlertController
+
+  async presentSuccessToast() {
+    const toast = await this.toastController.create({
+      message: 'Has creado tu cuenta con exito.',
+      duration: 2000,
+    });
+    toast.present();
+  }
+
+  async presentErrorAlert(message: string) {
+    const alert = await this.alertController.create({
+      header: 'StepMeter',
+      message: message,
+      buttons: ['Aceptar']
+    });
+    await alert.present();
+  }
+
+  formAccess() {
+    if (!this.user || !this.password || !this.rePassword) {
+      this.presentErrorAlert('Debes completar todos los campos para registrarte');
+      return;
+    }
+  
+    if (this.rePassword === this.password) {
+      console.log("SUCCESS: Login has been completed");
+      this.presentSuccessToast();
+      setTimeout(() => {
+      this.route.navigate(['/home']);
+      });
+    } else {
+      console.log("ERROR: Password is not the same to 'Confirm password'");
+      this.presentErrorAlert('Asegurate que al confirmar la contraseña sea la misma');
+    }
+  }
 
 }
